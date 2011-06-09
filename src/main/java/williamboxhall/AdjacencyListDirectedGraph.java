cbd26684d977f5426bc001c78843faaefd9245aa
williamboxhall@gmail.com <williamboxhall@gmail.com>
2011-06-09 14:42:01 +1000

    underlying adjacency lists can actually be sets...

--- src/main/java/williamboxhall/AdjacencyListDirectedGraph.java
+++ src/main/java/williamboxhall/AdjacencyListDirectedGraph.java
@@ -1,13 +1,12 @@
 package williamboxhall;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class AdjacencyListDirectedGraph implements Graph {
-    private Map<String, List<String>> nodeToAdjacencyList = new HashMap<String, List<String>>();
+    private Map<String, Set<String>> nodeToAdjacencyList = new HashMap<String, Set<String>>();
 
     public boolean adjacent(String source, String destination) {
         return adjacencyListFor(source).contains(destination);
@@ -17,9 +16,9 @@ public class AdjacencyListDirectedGraph implements Graph {
         adjacencyListFor(source).add(destination);
     }
 
-    private List<String> adjacencyListFor(String source) {
+    private Set<String> adjacencyListFor(String source) {
         if (!nodeToAdjacencyList.containsKey(source)) {
-            nodeToAdjacencyList.put(source, new ArrayList<String>());
+            nodeToAdjacencyList.put(source, new HashSet<String>());
         }
         return nodeToAdjacencyList.get(source);
     }
